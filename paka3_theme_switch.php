10da17decda391bec56eb8936308f832d960ce8c
Shoji ENDO <shojiendo@gmail.com>
2014-07-02 01:16:45 +0900

    必要な部分だけのこして、後の関数は削除。*全コードを確認したい場合は、最初のバージョンを確認。

--- paka3_theme_switch.php
+++ paka3_theme_switch.php
@@ -1,32 +1,18 @@
 <?php
 class Paka3_theme_switch {
 	private $name ;
-	private $dir ;
-	private $url ;
 
-	function __construct($name = null, $dir = null, $url = null) {
+	function __construct($name = null) {
 		$this->name = $name;
-		$this->dir  = $dir;
-		$this->url  = $url;
+
 		add_filter("show_admin_bar", "__return_false");
 		add_filter("stylesheet", array($this, "template"));
 		add_filter("template", array($this, "template"));
-		add_filter("theme_root", array($this, "theme_root"));
-		add_filter("theme_root_uri", array($this, "theme_root_uri"));
 	}
 	
 	public function template($name) {
 		$template_name = $this->name ? $this->name : $name; 
 		return $template_name;
 	}
-	
-	public function theme_root($dir) {
-		$dir_path = $this->dir ? $this->dir : $dir;
-		return $dir_path;
-	}
-	
-	public function theme_root_uri($url) {
-		$theme_url = $this->url ? $this->url : $url;
-		return $theme_url ;
-	}
+
 }
